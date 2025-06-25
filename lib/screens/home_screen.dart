e78963dd366eb9d16dd3fffe2d13a2b53356a29e
4a756c6961 <julia.jessing@iubh-fernstudium.de>
2025-06-25 19:37:46 +0200

    new project

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startseite'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.exit_to_app),
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+          ),
+        ],
+      ),
+      body: const Center(child: Text('Willkommen bei CampusConnect!')),
+    );
+  }
+}
