a4f2f9ed36cf7a616d5d549df19afe93c06d0e3d
4a756c6961 <julia.jessing@iubh-fernstudium.de>
2025-06-29 17:39:30 +0200

    changes

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,1 +1,8 @@
+import 'package:campus_connect/screens/auth_screen.dart';
+import 'package:flutter/cupertino.dart';
 
+class Auth with ChangeNotifier {
+  String _userID;
+
+  Future<void> signup(String email, String password) async {}
+}
