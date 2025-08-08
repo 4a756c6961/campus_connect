4dc5dab779df4f04ac173b838f3922de8b19fc6d
4a756c6961 <julia.jessing@iubh-fernstudium.de>
2025-08-08 14:17:02 +0200

    signUp

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,3 +1,15 @@
 import 'package:flutter/cupertino.dart';
 
-class Auth with ChangeNotifier {}
+class Auth with ChangeNotifier {
+String _token ;
+DateTime _expiryDate;
+String _userId;
+
+
+Future<void> signup(String email, String password) async {
+
+const url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY'
+
+}
+
+}
