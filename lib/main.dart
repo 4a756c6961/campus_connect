b789790b4c8ef754e1ee12de291a201a4de79b8b
4a756c6961 <julia.jessing@iubh-fernstudium.de>
2026-02-15 18:14:13 +0100

    Nutzer können sich endlich anmelden :D

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
-
+import 'dart:async';
 import 'firebase_options.dart';
 import 'providers/auth.dart';
 import 'screens/auth_gate.dart';
